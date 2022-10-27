077d950347b266b40ce3700b6c9773844b0f77a7
Mohammad Mu'izzuddin <76739315+mohdmuiz11@users.noreply.github.com>
2022-10-27 17:30:29 +0800

    Grid system improvement

---
+++ Assets/Scripts/GridSlot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GridSlot : XRSocketInteractor
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
